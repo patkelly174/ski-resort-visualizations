d91f52ebcee88d5af7405c50f22083309c1a34fb
patkelly174 <patkelly174@gmail.com>
2022-03-29 20:27:03 -0400

    added zoom

--- v1.1/visualization.js
+++ v1.1/visualization.js
@@ -1,45 +1,53 @@
 //Width and height of map
-var width = 960;
-var height = 500;
+const width = 960;
+const height = 500;
+
+const zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
 
 // D3 Projection
-var projection = d3.geo.albersUsa()
+const projection = d3.geoAlbersUsa()
                 .translate([width/2, height/2])    // translate to center of screen
                 .scale([1000]);          // scale things down so see entire US
 
 // Define path generator
-var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
+const path = d3.geoPath()               // path generator that will convert GeoJSON to SVG paths
 		  	 .projection(projection);  // tell path generator to use albersUsa projection
 
-//Create SVG element and append map to the SVG
-var svg = d3.select("body")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height);
 
-var div = d3.select("body")
+
+//Create SVG element and append map to the SVG
+const svg = d3.select("body")
+            .append("svg")
+            .attr("viewBox", [0, 0, width, height]);
+
+
+const g = svg.append("g");
+
+
+const div = d3.select("body")
     .append("div")   
     .attr("class", "tooltip")               
     .style("opacity", 0);
 
-d3.json("us-states.json", function (json) {
 
-
+d3.json("us-states.json").then(function(json) {
     // Bind the data to the SVG and create one path per GeoJSON feature
-    svg.selectAll("path")
+    // console.log(json.features);
+    const states = g.append("g")
+        .attr("fill", "#444")
+        .attr("cursor", "pointer")
+        .selectAll("path")
         .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("stroke", "#fff")
-        .style("stroke-width", "1")
-    	.style("fill", "gray");
+        .join("path")
+        .on("click", clicked)
+        .attr("d", path);
+
 
     
-    // Map the cities I have lived in!
-    d3.csv("ski_resort_stats.csv", function(data) {
-
-        svg.selectAll("circle")
+    d3.csv("ski_resort_stats.csv").then(function (data) {
+        g.selectAll("circle")
             .data(data)
             .enter()
             .append("circle")
@@ -53,22 +61,49 @@ d3.json("us-states.json", function (json) {
                 .style("fill", "blue")	
                 .style("opacity", 0.85)	
 
-            .on("mouseover", function(d) {      
+            .on("mouseover", function(event, d) {      
                 div.transition()        
                     .duration(200)      
                     .style("opacity", .9);      
                     div.text(d.resort_name)
-                    .style("left", (d3.event.pageX) + "px")     
-                    .style("top", (d3.event.pageY - 28) + "px");    
+                    .style("left", (event.pageX) + "px")     
+                    .style("top", (event.pageY - 28) + "px");    
             })  
             // fade out tooltip on mouse out               
-            .on("mouseout", function(d) {       
+            .on("mouseout", function(event, d) {       
                 div.transition()        
                 .duration(500)      
                 .style("opacity", 0);   
             }); 
-        
-        });  
-
-
+    });
 });
+
+function clicked(event, d) {    
+    const [[x0, y0], [x1, y1]] = path.bounds(d);
+    d3.select(this).transition().style("fill", "green");
+    svg.transition().duration(750).call(
+        zoom.transform,
+        d3.zoomIdentity
+          .translate(width / 2, height / 2)
+          .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+          .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+        d3.pointer(event, svg.node())
+      );
+}
+
+// function reset() {
+//     svg.selectAll("g").transition().style("fill", "#444");
+//     svg.transition().duration(750).call(
+//       zoom.transform,
+//       d3.zoomIdentity,
+//       d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
+//     );
+//   }
+
+function zoomed(event) {
+    const {transform} = event;
+    g.attr("transform", transform);
+    g.attr("stroke-width", 1 / transform.k);
+  }
+
+
