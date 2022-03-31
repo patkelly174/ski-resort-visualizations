0ffd4b27b00b05ad97b5c94cd44dc6b46eba41a6
Ross Brancati <79770198+rossbrancati@users.noreply.github.com>
2022-03-31 16:04:32 -0400

    Added skiable acres

--- v1.1/visualization.js
+++ v1.1/visualization.js
@@ -68,7 +68,8 @@ d3.json("us-states.json").then(function(json) {
                 tooltip.transition()        
                     .duration(200)      
                     .style("opacity", .9);
-		    tooltip.text("Resort Name: " + d.resort_name + "\n" 
+		    tooltip.text("Resort Name: " + d.resort_name + "\n"
+			+ "Skiable Acres: " + d.acres + "\n"
                         + "Summit Height: " + d.summit + "\n"
                         +"Vertical Drop: " + d.vertical + "\n"
                         +"Lifts: " + d.lifts + "\n")
